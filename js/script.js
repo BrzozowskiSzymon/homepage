63922ecf3ea3a3590549568022c4195fe24f8a99
Szymon Brzozowski <szymon.brzozi@gmail.com>
2023-08-17 15:40:04 +0200

    added new page

--- js/script.js
+++ js/script.js
@@ -1,38 +1,11 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+console.log("Hellow everbody!")
 
-let rateEUR = 4.4621;
-let rateGBP = 5.2091;
-let rateUSD = 4.0832;
-let rateTHB = 0.1156;
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".js-body");
+let themeName = document.querySelector(".js-themeName");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+button.addEventListener("click", () => {
+    body.classList.toggle("boody--dark");
+    nextColorName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-
-    let result;
-
-    switch (currency) {
-        case "EUR":
-            result = amount / rateEUR;
-            break;
-
-        case "GBP":
-            result = amount / rateGBP;
-            break;
-
-        case "USD":
-            result = amount / rateUSD;
-            break;
-
-        case "THB":
-            result = amount / rateTHB;
-            break;
-    }
-
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-});
+    });
